3cf89ef7fa6d55e1a289ae52f5999829b7623819
Mary Sigmon <marysigmon@Marys-MacBook-Pro-2.local>
2023-12-26 22:09:10 -0500

    initial

---
+++ src/App.jsx
@@ -0,0 +1,51 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import { Routes, Route, Link } from "react-router-dom";
+import Red from '.Red.jsx'
+import Blue from '.Blue.jsx'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div id="container">
+        <h1>Hello React Router!</h1>
+        <div id="navbar">{/* navigation here */}</div>
+        <div id="main-section">{/* routes here */}</div>
+      </div>
+
+      <div id="main-section">
+        <Routes>
+          <Route path="/blue" element={<h1>Blue</h1>} />
+          <Route path="/red" element={<h1>Red</h1>} />
+        </Routes>
+      </div>
+
+      <div>
+        <a href="https://vitejs.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  )
+}
+
+export default App
